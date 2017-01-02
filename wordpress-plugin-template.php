bf48c59817f8da3d89dd552859db54940e4a3347
carl alberto <cralberto11@gmail.com>
2017-01-02 18:08:24 +0800

    adding to the group repo

---
+++ wordpress-plugin-template.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Plugin Name: WordPress Plugin Template
+ * Version: 1.0
+ * Plugin URI: https://carl.alber2.com/
+ * Description: This is your starter template for your next WordPress plugin.
+ * Author: Carl Alberto
+ * Author URI: https://carl.alber2.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: wordpress-plugin-template
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Carl Alberto
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Load plugin class files
+require_once( 'includes/class-wordpress-plugin-template.php' );
+require_once( 'includes/class-wordpress-plugin-template-settings.php' );
+
+// Load plugin libraries
+require_once( 'includes/lib/class-wordpress-plugin-template-admin-api.php' );
+require_once( 'includes/lib/class-wordpress-plugin-template-post-type.php' );
+require_once( 'includes/lib/class-wordpress-plugin-template-taxonomy.php' );
+
+/**
+ * Returns the main instance of WordPress_Plugin_Template to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object WordPress_Plugin_Template
+ */
+function WordPress_Plugin_Template () {
+	$instance = WordPress_Plugin_Template::instance( __FILE__, '1.0.0' );
+
+	if ( is_null( $instance->settings ) ) {
+		$instance->settings = WordPress_Plugin_Template_Settings::instance( $instance );
+	}
+
+	return $instance;
+}
+
+WordPress_Plugin_Template();
